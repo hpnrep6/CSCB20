2cc9e48231c510c207d15556d6f68165e9adfdab
hpnrep6 <hpnrep6@gmail.com>
2022-03-03 10:19:56 -0500

    Add Question1

---
+++ Question1.py
@@ -0,0 +1,39 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<name>')
+def user(name):
+    return format(name)
+
+def format(input: str) -> str:
+    input.strip()
+    hasUpper = False
+    hasLower = False
+
+    formatted = ""
+
+    for i in range(len(input)):
+        if input[i].islower():
+            hasLower = True
+        elif input[i].isupper():
+            hasUpper = True
+        
+        if input[i].isalpha():
+            formatted += input[i]
+    
+    if hasLower and hasUpper:
+        # Kind of forgot if python actually handles invalid indicies but like
+        # lets just assume it doesn't because why not
+        if len(formatted) >= 1:
+            formatted = formatted[0].upper() + formatted[1:]
+        if len(formatted) >= 2:
+            formatted = formatted[0] + formatted[1:].lower()
+    elif hasLower or hasUpper:
+        formatted = formatted.swapcase()
+    
+    return formatted
+
+
+
+
+user('12AbSDA3')
